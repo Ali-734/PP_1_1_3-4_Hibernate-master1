cc7716a819a43a811e6ff8186da0ec853869bc4e
Ali-734 <ali2004mah@mail.ru>
2024-07-08 16:48:31 +0300

    ebo3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,20 +3,22 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
 import org.hibernate.query.Query;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private final SessionFactory sessionFactory;
 
+    public UserDaoHibernateImpl() {
+        this.sessionFactory = Util.getSessionFactory();
     }
 
     @Override
     public void createUsersTable() {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             String sql = "CREATE TABLE IF NOT EXISTS users (" +
                     "id BIGINT NOT NULL AUTO_INCREMENT, " +
@@ -36,7 +38,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             String sql = "DROP TABLE IF EXISTS users";
             Query query = session.createSQLQuery(sql);
@@ -52,7 +54,7 @@ public class UserDaoHibernateImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.save(user);
             transaction.commit();
@@ -65,12 +67,14 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             User user = session.get(User.class, id);
-            session.delete(user);
+            if (user != null) {
+                session.delete(user);
+            }
             transaction.commit();
-        }catch (Exception e) {
+        } catch (Exception e) {
             if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
@@ -78,21 +82,22 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        try (Session session = Util.getSessionFactory().openSession()) {
+        List<User> users = null;
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             Query<User> query = session.createQuery("FROM User", User.class);
-            List<User> users = query.getResultList();
+            users = query.getResultList();
             session.getTransaction().commit();
-            return users;
         } catch (Exception e) {
-            throw new RuntimeException("Error retrieving users: " + e.getMessage(), e);
+            e.printStackTrace();
         }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             String sql = "DELETE FROM users";
             session.createNativeQuery(sql).executeUpdate();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,15 @@
 package jm.task.core.jdbc.util;
 
-import org.hibernate.Session;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class Util {
-    private final static String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=UTC";
-    private final static String USERNAME = "root";
-    private final static String PASSWORD = "Alishka12345";
-    private final static Logger LOGGER = Logger.getLogger(Util.class.getName());
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Alishka12345";
+    private static final Logger LOGGER = Logger.getLogger(Util.class.getName());
 
     private static SessionFactory sessionFactory;
 
@@ -21,7 +17,7 @@ public class Util {
         try {
             Configuration configuration = new Configuration().addAnnotatedClass(User.class);
             configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
             configuration.setProperty("hibernate.connection.url", URL);
             configuration.setProperty("hibernate.connection.username", USERNAME);
             configuration.setProperty("hibernate.connection.password", PASSWORD);
